f957c9a6aa45f968df009d0f9d4771636e62a470
nmhillusion <nguyenminhhieu.geek@gmail.com>
2022-12-17 21:22:43 +0700

    astart program by command file

---
+++ start_program.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo.
+echo Preparing environment...
+echo.
+
+pip install -r requirements.txt
+
+echo.
+echo :: CatRunner :: Send Template Mail ::
+echo.
+
+pause
+
+python .
+
+echo.
+pause
